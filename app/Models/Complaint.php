19e1ebb7bf57c3501f235e03dce0546b1d7bc89d
Libeddd <abidkelengkeng1234@gmail.com>
2026-04-26 14:05:24 +0700

    fitur: Pembuatan model Eloquent dan relasi antar tabel

---
+++ app/Models/Complaint.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Complaint extends Model
+{
+    protected $fillable = [
+        'complaint_code',
+        'user_id',
+        'order_number',
+        'product_name',
+        'damage_category',
+        'description',
+        'refund_method',
+        'proof_image_path',
+        'unboxing_video_path',
+        'status',
+        'completed_at',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
